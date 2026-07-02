f0df481aafab0cb63119d6c78492b6b3c6a5136b
Justin Perkins <76526205+xArumos@users.noreply.github.com>
2026-07-02 01:43:14 +0000

    [LeetCode Sync] Runtime - 7 ms (63.43%), Memory - 19.3 MB (53.70%)

--- DSA/0009-palindrome-number/solution.py
+++ DSA/0009-palindrome-number/solution.py
@@ -1,6 +1,17 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        if str(x) == str(x)[::-1]:
-            return True
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        converted = str(x)
+
+        print(converted[0:(len(converted) // 2)])
+        print(converted[(len(converted) // 2):])
+
+        if (len(converted) % 2 == 0):
+            if (converted[0:(len(converted) // 2)] == converted[(len(converted) // 2):][::-1]):
+                return True
+            else:
+                return False
         else:
-            return False
+            if (converted[0:(len(converted) // 2)] == converted[(len(converted) // 2 + 1):][::-1]):
+                return True
+            else:
+                return False
