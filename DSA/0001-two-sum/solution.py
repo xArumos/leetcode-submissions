319507edb287850d69422c02587397b4e9f645e8
Justin Perkins <76526205+xArumos@users.noreply.github.com>
2026-07-02 01:08:54 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 20.5 MB (40.28%)

--- DSA/0001-two-sum/solution.py
+++ DSA/0001-two-sum/solution.py
@@ -1,10 +1,20 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        for i, j in enumerate(nums):
-            for k, l in enumerate(nums):
-                if i == k:
-                    pass
-                else:
-                    if j + l == target:
-                        return [i, k]
-        return []
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        visited = {}
+
+        for i, num in enumerate(nums):
+            compliment = target - num
+
+            if (compliment in visited and compliment + num == target):
+                return [i, visited[compliment]]
+            
+            visited[num] = i
+
+
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i, iNum in enumerate(nums):
+#             for j, jNum in enumerate(nums):
+#                 if (i != j and iNum + jNum == target):
+#                     return [i, j]
